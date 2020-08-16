50c385b6968457d93a56813779ca38b500fe721a
Rob Cherny <rcherny@gmail.com>
2020-08-16 10:45:07 -0400

    Comments, examples etc.

--- example/log.js
+++ example/log.js
@@ -2,25 +2,25 @@ import { logTool } from '../src/index.js';
 
 let log = logTool();
 
-log('This is the external logger');
+log('This is the external logger'); // 'This is the external logger'
 
 setTimeout(() => {
-   log('This is a delayed log');
+   log('This is a delayed log'); // 'This is a delayed log'
    setTimeout(() => {
-      log('Disable the next logger.');
-      log = logTool(false);
-      log('Disabled');
+      log('Disable the next logger.'); // 'Disable the next logger.'
+      log = logTool(false); 
+      log('Are we logging?'); // disabled
    }, 2000)
 }, 2000)
 
-const debug = logTool(false)
+const debug = logTool(false);
 
-debug('Testing debug call that should be disabled.');
+debug('Testing debug call that should be disabled.'); // disabled
 
 const bugger = logTool(true, {
    namespace: 'Debugger:',
    method: 'debug'
-})
+});
 
-bugger('This one is running.');
+bugger('This one is running.'); // 'This one is running.'
 
